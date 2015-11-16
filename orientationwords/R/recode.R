55a8a5acc211cdd4c95347fb8944896c81dfaebd
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-11-16 07:10:21 -0600

    Predicted results

---
+++ orientationwords/R/recode.R
@@ -0,0 +1,38 @@
+#' Recode all variables for this experiment.
+#'
+#' @param frame data.frame to recode.
+#' @importFrom dplyr `%>%`
+#' @export
+recode <- function(frame) {
+  frame %>%
+    recode_cue_type %>%
+    recode_mask_type %>%
+    recode_response_type
+}
+
+recode_cue_type <- function(frame) {
+  cue_type_map <- dplyr::data_frame(
+    cue_type = c("invalid", "valid"),
+    cue_c = c(-0.5, 0.5)
+  )
+  try(frame <- dplyr::left_join(frame, cue_type_map))
+  frame
+}
+
+recode_mask_type <- function(frame) {
+  mask_type_map <- dplyr::data_frame(
+    mask_type = c("nomask", "mask"),
+    mask_c = c(-0.5, 0.5)
+  )
+  try(frame <- dplyr::left_join(frame, mask_type_map))
+  frame
+}
+
+recode_response_type <- function(frame) {
+  response_type_map <- dplyr::data_frame(
+    response_type = c("pic", "word"),
+    response_label = c("Upright picture", "Verify word")
+  )
+  try(frame <- dplyr::left_join(frame, response_type_map))
+  frame
+}
---
+++ orientationwords/R/theme.R
@@ -0,0 +1,15 @@
+scale_x_mask <- ggplot2::scale_x_continuous("", breaks = c(-0.5, 0.5),
+                                            labels = c("No mask", "Mask"))
+
+scale_y_rt <- ggplot2::scale_y_continuous("Reaction Time")
+
+cue_colors <- list(invalid = "#d7191c", valid = "#a6d96a")
+scale_color_cue_type <- ggplot2::scale_color_manual("",
+                                                    labels = c("Invalid", "Valid"),
+                                                    values = unlist(cue_colors))
+
+base_theme <- ggplot2::theme_minimal() +
+  theme(
+    axis.ticks = element_blank(),
+    panel.margin = grid::unit(2, "lines")
+  )
