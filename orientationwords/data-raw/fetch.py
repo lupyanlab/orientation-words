22c8e744ed5fb7e6f48ac1e57a3ac8e904be9ccc
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-11-16 00:02:56 -0600

    Added subj info fetcher

---
+++ orientationwords/data-raw/fetch.py
@@ -0,0 +1,20 @@
+import json
+import gspread
+import pandas
+from oauth2client.client import SignedJwtAssertionCredentials as Credentials
+
+json_key = json.load(open('drive-api-creds.json'))
+scope = ['https://spreadsheets.google.com/feeds', ]
+
+credentials = Credentials(json_key['client_email'],
+                          json_key['private_key'].encode(),
+                          scope)
+gc = gspread.authorize(credentials)
+
+workbook = gc.open('MWP Subject Info')
+worksheet = workbook.worksheet('orientation-words')
+records = worksheet.get_all_records()
+subj_info = pandas.DataFrame.from_records(records)
+subj_info = subj_info[worksheet.row_values(1)]  # set column order
+subj_info.to_csv('subj_info.csv', index=False)
+
