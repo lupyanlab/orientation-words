ba02b8c4c4befe35dd9d500e22b3088e8af0ff1a
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-11-24 15:01:59 -0600

    Pic only mod with subj analysis

--- models/unilateral.R
+++ models/unilateral.R
@@ -7,6 +7,29 @@ library(devtools)
 load_all("orientationwords")
 data(unilateral)
 
+# ---- subjs
+subjs <- unilateral %>%
+  group_by(subj_id) %>%
+  summarize(
+    rt = mean(rt, na.rm = TRUE),
+    error = mean(is_error, na.rm = TRUE)
+  ) %>%
+  mutate(
+    rank_rt = rank(rt),
+    rank_error = rank(error)
+  )
+ggplot(subjs, aes(x = rank_error, y = error)) +
+  geom_point() +
+  geom_text(aes(label = subj_id), hjust = -0.1, angle = 90) +
+  coord_cartesian(ylim = c(0, 0.30))
+
+# ---- pic-mod
+pic_mod <- lmer(rt ~ cue_c * mask_c + (1|subj_id),
+                data = filter(unilateral,
+                              subj_id != "MOW111",
+                              response_type == "pic"))
+summary(pic_mod)
+
 # ---- unilateral-mod
 overall <- lmer(rt ~ cue_c * mask_c * response_c + (1|subj_id),
                 data = unilateral)
