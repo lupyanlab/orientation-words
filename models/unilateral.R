39d9cd517f4c6ff018f28aab6ba7f161132fd4fd
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-11-25 22:12:17 -0600

    Cleaned up report for v1 and v2

--- models/unilateral.R
+++ models/unilateral.R
@@ -12,41 +12,41 @@ load_all("orientationwords")
 
 data(unilateral)
 
-# ---- subjs
-subjs <- unilateral %>%
-  group_by(subj_id) %>%
-  summarize(
-    rt = mean(rt, na.rm = TRUE),
-    error = mean(is_error, na.rm = TRUE)
-  ) %>%
-  mutate(
-    rank_rt = rank(rt),
-    rank_error = rank(error)
-  )
-ggplot(subjs, aes(x = rank_error, y = error)) +
-  geom_point() +
-  geom_text(aes(label = subj_id), hjust = -0.1, angle = 90) +
-  coord_cartesian(ylim = c(0, 0.30))
+# Version 1
 
-# ---- pic-mod
-pic_mod <- lmer(rt ~ cue_c * mask_c + (1|subj_id),
-                data = filter(unilateral,
-                              subj_id != "MOW111",
-                              response_type == "pic"))
-summary(pic_mod)
+# ---- v1-pic-mod
+v1_pic_mod <- lmer(rt ~ cue_c * mask_c + (1|subj_id),
+                   data = filter(unilateral, version == 1, response_type == "pic"))
+tidy(v1_pic_mod, effects = "fixed")
 
-# ---- unilateral1-mod
-unilateral1_mod <- lmer(rt ~ cue_c * mask_c * response_c + (1|subj_id),
-                        data = filter(unilateral, version == 1))
-tidy(unilateral1_mod, effects = "fixed")
+# ---- v1-word-mod
+v1_word_mod <- lmer(rt ~ mask_c + (1|subj_id),
+                    data = filter(unilateral, version == 1, response_type == "word"))
+tidy(v1_word_mod, effects = "fixed")
 
-# ---- error-mod
-overall_error <- glmer(is_error ~ cue_c * mask_c * response_c + (1|subj_id),
-                       data = filter(unilateral, version == 1),
-                       family = binomial)
-tidy(overall_error, effects = "fixed")
+# ---- v1-task-mod
+v1_task_mod <- lmer(rt ~ cue_c * mask_c * response_c + (1|subj_id),
+                    data = filter(unilateral, version == 1))
+tidy(v1_task_mod, effects = "fixed")
 
-# ---- error-plot
+# ---- v1-plot
+ggplot(filter(unilateral, version == 1),
+       aes(x = mask_c, y = rt, color = cue_task)) +
+  geom_point(stat = "summary", fun.y = "mean") +
+  geom_line(stat = "summary", fun.y = "mean") +
+  facet_wrap("response_label") +
+  scale_x_mask +
+  scale_y_rt +
+  scale_color_unilateral_cue_task +
+  base_theme
+
+# ---- v1-error-mod
+v1_error_mod <- glmer(is_error ~ cue_c * mask_c * response_c + (1|subj_id),
+                      data = filter(unilateral, version == 1),
+                      family = binomial)
+tidy(v1_error_mod, effects = "fixed")
+
+# ---- v1-error-plot
 ggplot(filter(unilateral, version == 1),
        aes(x = mask_c, y = is_error, color = cue_task)) +
   geom_point(stat = "summary", fun.y = "mean") +
@@ -57,7 +57,7 @@ ggplot(filter(unilateral, version == 1),
   scale_color_unilateral_cue_task +
   base_theme
 
-# ---- error-type-plot
+# ---- v1-error-type-plot
 error_types <- unilateral %>%
   filter(version == 1) %>%
   group_by(response_type) %>%
@@ -72,43 +72,99 @@ ggplot(error_types, aes(x = response_type, y = error_rate, fill = error_type, or
   scale_y_error +
   base_theme
 
-# ---- unilateral2-mod
-unilateral2_mod <- lmer(rt ~ cue_c * mask_c * response_c + (1|subj_id),
-                        data = filter(unilateral, version == 2))
-tidy(unilateral2_mod, effects = "fixed")
+# Version 2
 
-# ---- unilateral1-plot
-ggplot(filter(unilateral, version == 1),
-       aes(x = mask_c, y = rt, color = cue_type)) +
-  geom_point(stat = "summary", fun.y = "mean") +
-  geom_line(stat = "summary", fun.y = "mean") +
-  facet_wrap("response_label") +
-  scale_x_mask +
-  scale_y_rt +
-  scale_color_cue_type +
-  base_theme
+# ---- v2-pic-mod
+v2_pic_mod <- lmer(rt ~ cue_c * mask_c + (1|subj_id),
+                   data = filter(unilateral, version == 2, response_type == "pic"))
+tidy(v2_pic_mod, effects = "fixed")
 
-# ---- unilateral2-plot
+# ---- v2-word-mod
+v2_word_mod <- lmer(rt ~ mask_c + (1|subj_id),
+                    data = filter(unilateral, version == 2, response_type == "word"))
+tidy(v2_word_mod, effects = "fixed")
+
+# ---- v2-task-mod
+v2_task_mod <- lmer(rt ~ cue_c * mask_c * response_c + (1|subj_id),
+                    data = filter(unilateral, version == 2))
+tidy(v2_task_mod, effects = "fixed")
+
+# ---- v2-plot
 ggplot(filter(unilateral, version == 2),
-       aes(x = mask_c, y = rt, color = cue_type)) +
+       aes(x = mask_c, y = rt, color = cue_task)) +
   geom_point(stat = "summary", fun.y = "mean") +
   geom_line(stat = "summary", fun.y = "mean") +
   facet_wrap("response_label") +
   scale_x_mask +
   scale_y_rt +
-  scale_color_cue_type +
+  scale_color_unilateral_cue_task +
   base_theme
 
-# ---- unilateral-mod
-unilateral_mod <- lmer(rt ~ cue_c * mask_c * response_c + (1|subj_id), data = unilateral)
-tidy(unilateral_mod, effects = "fixed")
+# ---- v2-error-plot
+ggplot(filter(unilateral, version == 2),
+       aes(x = mask_c, y = is_error, color = cue_task)) +
+  geom_point(stat = "summary", fun.y = "mean") +
+  geom_line(stat = "summary", fun.y = "mean") +
+  facet_wrap("response_label") +
+  scale_x_mask +
+  scale_y_error +
+  scale_color_unilateral_cue_task +
+  base_theme
 
-# ---- unilateral-plot
-ggplot(unilateral, aes(x = mask_c, y = rt, color = cue_type)) +
+# ---- v2-error-type-plot
+error_types <- unilateral %>%
+  filter(version == 2) %>%
+  group_by(response_type) %>%
+  summarize(
+    wrong_type = sum(error_type == "wrong_type", na.rm = TRUE)/n(),
+    wrong_key = sum(error_type == "wrong_key", na.rm = TRUE)/n()
+  ) %>%
+  gather(error_type, error_rate, -response_type)
+
+ggplot(error_types, aes(x = response_type, y = error_rate, fill = error_type, order = rev(error_type))) +
+  geom_bar(stat = "identity") +
+  scale_y_error +
+  base_theme
+
+# Overall analyses
+
+# ---- pic-mod
+pic_mod <- lmer(rt ~ cue_c * mask_c + (1|subj_id),
+                data = filter(unilateral, response_type == "pic"))
+tidy(pic_mod, effects = "fixed")
+
+# ---- word-mod
+word_mod <- lmer(rt ~ mask_c + (1|subj_id),
+                 data = filter(unilateral, response_type == "word"))
+tidy(word_mod, effects = "fixed")
+
+# ---- task-mod
+task_mod <- lmer(rt ~ cue_c * mask_c * response_c + (1|subj_id), data = unilateral)
+tidy(task_mod, effects = "fixed")
+
+# ---- overall-plot
+ggplot(unilateral, aes(x = mask_c, y = rt, color = cue_task)) +
   geom_point(stat = "summary", fun.y = "mean") +
   geom_line(stat = "summary", fun.y = "mean") +
   facet_wrap("response_label") +
   scale_x_mask +
   scale_y_rt +
-  scale_color_cue_type +
+  scale_color_unilateral_cue_task +
   base_theme
+
+# ---- subjs
+subjs <- unilateral %>%
+  group_by(subj_id) %>%
+  summarize(
+    rt = mean(rt, na.rm = TRUE),
+    error = mean(is_error, na.rm = TRUE)
+  ) %>%
+  mutate(
+    rank_rt = rank(rt),
+    rank_error = rank(error)
+  )
+
+ggplot(subjs, aes(x = rank_error, y = error)) +
+  geom_point() +
+  geom_text(aes(label = subj_id), hjust = -0.1, angle = 90) +
+  coord_cartesian(ylim = c(0, 0.30))
